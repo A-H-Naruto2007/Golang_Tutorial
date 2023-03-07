dd7288eff30f17dd1b1d9283aa6d405ae06fb8c1
A-H-NARUTO2007 <95155035+A-H-Naruto2007@users.noreply.github.com>
2023-03-07 11:39:14 +0600

    variable in golang.

---
+++ variable.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	var myName, country string
+	var age int
+	var pscResult int
+
+	myName = "A-H NARUTO"
+	country = "Bangladesh"
+	age = 16
+	pscResult = 5
+
+	println("Hello,", myName, "is crazy.")
+	println("My country is", country)
+	println("My age is", age)
+	println("I got", pscResult, "GPA in PSC.")
+}
