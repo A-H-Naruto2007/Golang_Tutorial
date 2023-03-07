bdb82e3adb6724656770275a374ec26fc0f3296c
A-H-NARUTO2007 <95155035+A-H-Naruto2007@users.noreply.github.com>
2023-03-07 11:48:29 +0600

    variable.go

--- variable.go
+++ variable.go
@@ -1,15 +1,22 @@
 package main
 
 func main() {
-	var myName, country string = "A-H NARUTO", "Bangladesh"
-	var age int = 16
-	var pscResult float32 = 5.0
-/*
-	myName = "A-H NARUTO"
-	country = "Bangladesh"
-	age = 16
-	pscResult = 5
-*/
+	/*
+		var myName, country = "A-H NARUTO", "Bangladesh"
+		var age = 16
+		var pscResult = 5.0
+	*/
+	/*
+		myName = "A-H NARUTO"
+		country = "Bangladesh"
+		age = 16
+		pscResult = 5
+	*/
+	myName := "A-H NARUTO"
+	country := "Bangladesh"
+	age := 16
+	pscResult := 5.0
+
 	println("Hello,", myName, "is crazy.")
 	println("My country is", country)
 	println("My age is", age)
